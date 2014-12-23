f529ccd0c1136943d21675acb5235542c1df288a
Bipin Mathew <bipinmathew@gmail.com>
2014-12-22 20:01:22 -0500

    Maybe this backprop works? need to work on it more.

--- main.m
+++ main.m
@@ -6,5 +6,7 @@ L = length(nn_structure);
 
 w = nn_initweights(nn_structure);
 
+Y = y==[0:9];
+
 o=nn_feedforward(X(1,:),w,nn_structure);
-jw = nn_backprop(y,o,nn_structure);
+jw = nn_backprop(Y(1,:),o,w,nn_structure);
--- nn_backprop.m
+++ nn_backprop.m
@@ -1,4 +1,21 @@
-function [jw] = nn_backprop(y,o,nn_structure)
+function [jw] = nn_backprop(y,o,w,nn_structure)
+    L = length(nn_structure);    
+    d = zeros(sum(nn_structure),1);
+    e = length(o);
+    we = length(w);
+
+    l = L;
+    d(e-nn_structure(l)+1:e)=(o(e-nn_structure(l)+1:e)-y.').*(1-o(e-nn_structure(l)+1:e)).*o(e-nn_structure(l)+1:e);
+
+    for l=(L-1):-1:1
+        temp=reshape(w(we-(1+nn_structure(l))*(nn_structure(l+1))+1:we),nn_structure(l+1),1+nn_structure(l))'*d(e-nn_structure(l+1)+1:e);
+
+        we -=(1+nn_structure(l))*(nn_structure(l+1));
+        e = e-nn_structure(l+1);
+        d(e-nn_structure(l)+1:e+1)=temp;
+    end
+    
+    
     disp("Stopped in nn_backprop");
     keyboard;
 end
