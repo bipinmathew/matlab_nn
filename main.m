2332e3c8e7d44feed9c361d73aa9445235dd60b1
Bipin Mathew <bipinmathew@gmail.com>
2015-01-01 16:10:50 -0500

    Merging in further changes

--- main.m
+++ main.m
@@ -1,12 +1,44 @@
 [X,y]=read_mnist('../data/train.csv');
 [M,N]=size(X);
+alpha = 0.1;
 K = length(unique(y));
 nn_structure=[N,5,K];
 L = length(nn_structure);
 
 w = nn_initweights(nn_structure);
+grad = zeros(length(w),1);
 
 Y = y==[0:9];
 
-o=nn_feedforward(X(1,:),w,nn_structure);
-jw = nn_backprop(Y(1,:),o,w,nn_structure);
+J=zeros(M,1);
+
+
+for i=1:M
+    w = w-alpha*grad;
+    
+    costFunc = @(w) nn_train(X(i,:),Y(i,:),w,nn_structure);
+    [j,grad] = costFunc(w);
+
+    J(i)=j;
+end
+
+
+    numgrad = computeNumericalGradient(costFunc, w);
+
+    disp([numgrad grad]);
+    fprintf(['The above two columns you get should be very similar.\n' ...
+             '(Left-Your Numerical Gradient, Right-Analytical Gradient)\n\n']);
+
+    % Evaluate the norm of the difference between two solutions.  
+    % If you have a correct implementation, and assuming you used EPSILON = 0.0001 
+    % in computeNumericalGradient.m, then diff below should be less than 1e-9
+    diff = norm(numgrad-grad)/norm(numgrad+grad);  
+     
+    fprintf(['If your backpropagation implementation is correct, then \n' ...
+             'the relative difference will be small (less than 1e-9). \n' ...
+             '\nRelative Difference: %g\n'], diff);
+
+
+
+
+
--- nn_backprop.m
+++ nn_backprop.m
@@ -1,15 +1,19 @@
-function [jw] = nn_backprop(y,o,w,nn_structure)
+function [j,jw] = nn_backprop(y,o,w,nn_structure)
     L = length(nn_structure);    
-    d = zeros(sum(nn_structure),1);
+    d = zeros(sum(nn_structure(2:end)),1);
     e = length(d);
     we = length(w);
     w_size=length(w);
     jw = zeros(w_size,1);
 
     l = L;
-    d(e-nn_structure(l)+1:e)=(o(e-nn_structure(l)+1:e)-y.').*(1-o(e-nn_structure(l)+1:e)).*o(e-nn_structure(l)+1:e);
 
-    for l=(L-1):-1:1
+
+    j = (norm((o(end-nn_structure(l)+1:end)-y.'))^2)/2;
+
+    d(end-nn_structure(l)+1:end)=(o(end-nn_structure(l)+1:end)-y.').*(1-o(end-nn_structure(l)+1:end)).*o(end-nn_structure(l)+1:end);
+
+    for l=(L-1):-1:2
         W=reshape(w(we-(1+nn_structure(l))*(nn_structure(l+1))+1:we),nn_structure(l+1),1+nn_structure(l))'(2:end,:);
         temp=(W*d(e-nn_structure(l+1)+1:e));
 
@@ -19,16 +23,13 @@ function [jw] = nn_backprop(y,o,w,nn_structure)
         d(e-nn_structure(l)+1:e)=temp.*(1-o(e-nn_structure(l)+1:e)).*o(e-nn_structure(l)+1:e);
     end
 
-
     ai=[0,cumsum(nn_structure)];
+    di=[0,cumsum(nn_structure(2:end))];
     ws=0;
 
     for l=1:L-1
         w_size =  (1+nn_structure(l))*nn_structure(l+1);
-        jw(1+ws:ws+w_size)=kron([1;o(1+ai(l):ai(l+1))],d(1+ai(l):ai(l+1)))
+        jw(1+ws:ws+w_size)=kron([1;o(1+ai(l):ai(l+1))],d(1+di(l):di(l+1)));
         ws += w_size;
     end
-
-    disp("Stopped")
-    keyboard;    
 end
