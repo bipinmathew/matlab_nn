a6474585aa7faa14eff3a3fe99f96476d3817aac
Bipin Mathew <bipinmathew@gmail.com>
2015-02-28 17:02:16 -0500

    Checking in main.m

--- main.m
+++ main.m
@@ -6,12 +6,20 @@ warning('off', 'Octave:possible-matlab-short-circuit-operator');
 rand("seed",1);
 
 [X,y]=read_mnist('../data/train.csv');
-%X=X(1:1000,:);
-%y=y(1:1000);
+%X=X(1:8000,:);
+%y=y(1:8000);
 
-[M,N]=size(X);
+[M_f,N_f]=size(X);
 
-breaks=[0,cumsum(M*[0.60,0.30,0.10])];
+NUMPC = 10;
+[v,mu] = pca(X(1:1000,:));
+
+
+v = v(:,1:NUMPC);
+
+X=(X-mu)*v;
+
+breaks=[0,cumsum(M_f*[0.60,0.30,0.10])];
 
 X_tr=X(1+breaks(1):breaks(2),:);
 y_tr=y(1+breaks(1):breaks(2))(:);
@@ -24,6 +32,7 @@ y_t=y(1+breaks(3):breaks(4))(:);
 
 
 [Xi] = read_mnist_test('../data/test.csv');
+Xi = (Xi-mu)*v;
 
 K = length(unique(y));
 
@@ -42,19 +51,15 @@ K = length(unique(y));
 %y_tr = yss(:)(p);
 
 
+
 [M,N]=size(X_tr);
 
 NUMEPOCHS = 10;
-BLOCKSIZE = 10;
+BLOCKSIZE = 1000;
 NUMBLOCKS = floor(M/BLOCKSIZE);
 
 alpha = 0.1;
 lambda = 0.1;
-num_epochs = 1;
-
-mu = mean(X_tr,1);
-sigma= 1+std(X_tr,1);
-
 
 e_cv_max = Inf;
 mc_cv_max = 1;
@@ -70,13 +75,14 @@ grad = zeros(length(w_init),1);
 
 y_sample=zeros(length(y_tr),1);
 
-Y = y_tr==[0:K-1];
+Y_tr = y_tr==[0:K-1];
+Y = y==[0:K-1];
 
 w_opt = w_init;
 
 e_cv_max = Inf;
 
-lambdas= 10.^[-1:1:1]; %10.^[-4:0.5:-1]; %10.^[1];
+lambdas= 10.^[-2:0.25:1]; %10.^[-4:0.5:-1]; %10.^[1];
 
 e_tr = zeros(length(lambdas),1);
 e_cv = zeros(length(lambdas),1);
@@ -91,31 +97,47 @@ for lambda=lambdas
         for j=0:NUMBLOCKS-1
             disp(sprintf("... Processing block: %d of %d \r\n",j+1,NUMBLOCKS));
             idx = [(j*BLOCKSIZE)+1:1:min((j+1)*BLOCKSIZE,M)];
-            costFunc = @(w) nn_train((X_tr(idx,:)-mu)./sigma,Y(idx,:),w,lambda,nn_structure);
-
-            options = optimset('MaxIter', 500);
-            [w, cost] = fmincg(costFunc, w, options);
-
+            
+            [j,jw]=nn_train(X_tr(idx,:),Y_tr(idx,:),w,lambda,nn_structure);
+            disp(sprintf("cost: %f\r\n",j));
+            w -= alpha*jw;
         end
     end
-        yp_tr = nn_test((X_tr(idx,:)-mu)./sigma,w,nn_structure)(:)-1;
-        yp_cv = nn_test((X_cv-mu)./sigma,w,nn_structure)(:)-1;
+        yp_tr = nn_test(X_tr(idx,:),w,nn_structure)(:)-1;
+        yp_cv = nn_test(X_cv,w,nn_structure)(:)-1;
         e_tr(k) = sum((y_tr(idx)!=yp_tr))/length(idx);
         e_cv(k) = sum((y_cv!=yp_cv))/length(y_cv);
         disp(sprintf("lambda: %f, e_tr error: %f, e_cv: %f \r\n",lambda,e_tr(k),e_cv(k)));
-        k+=1;
-    if(e_cv<e_cv_max)
-        e_cv_max = e_cv;
+    if(e_cv(k)<e_cv_max)
+        e_cv_max = e_cv(k);
         w_opt = w;
         best_lambda = lambda;
     end
+    k+=1;
 end
 
-yp_t = nn_test((X_t-mu)./sigma,w_opt,nn_structure)(:)-1;
+
+w = w_init;
+NUMBLOCKS = floor(M_f/BLOCKSIZE);
+for epoch=1:NUMEPOCHS
+    disp(sprintf("Starting epoch: %d\r\n",epoch))
+    for j=0:NUMBLOCKS-1
+        disp(sprintf("... Processing block: %d of %d \r\n",j+1,NUMBLOCKS));
+        idx = [(j*BLOCKSIZE)+1:1:min((j+1)*BLOCKSIZE,M_f)];
+        
+        [j,jw]=nn_train(X(idx,:),Y(idx,:),w,best_lambda,nn_structure);
+        disp(sprintf("cost: %f\r\n",j));
+        w -= alpha*jw;
+    end
+end
+w_opt = w;
+
+yp_t = nn_test(X_t,w_opt,nn_structure)(:)-1;
 e_t = sum(yp_t!=y_t)/length(y_t)
 
-%yp_tr = nn_test((X_tr-mu)./sigma,w,nn_structure)(:)-1;
-%yp_cv = nn_test((X_cv-mu)./sigma,w,nn_structure)(:)-1;
+disp(sprintf("Test error: %f\r\n",e_t));
 
+disp("Writing out result...\r\n");
 
-
+yp_i = nn_test(Xi,w_opt,nn_structure)(:)-1;
+dlmwrite('output.csv',[[1:1:length(yp_i)].',yp_i],'delimiter',',');
